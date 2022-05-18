7ce0683ead4a054cfebb28983017efd3d735b2c2
Morgan Laversin <morganlaversin@gmail.com>
2022-05-18 10:19:42 +0200

    implementation of sauce route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const path = require('path')
 
 const userRoutes = require('./routes/user')
+const sauceRoutes = require('./routes/sauce')
 
 const app = express()
 
@@ -33,5 +34,6 @@ app.use((req, res, next) => {
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 app.use('/api/auth', userRoutes)
+app.use('/api/sauces', sauceRoutes)
 
 module.exports = app
