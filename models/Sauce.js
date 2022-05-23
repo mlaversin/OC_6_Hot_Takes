914b32a1c2105686865332e6f17f995629b9a40e
Morgan Laversin <morganlaversin@gmail.com>
2022-05-23 14:40:42 +0200

    config of mongoose-errors package

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const mongooseErrors = require('mongoose-errors')
 
 const sauceSchema = mongoose.Schema({
   userId: { type: String, required: true },
@@ -13,5 +14,7 @@ const sauceSchema = mongoose.Schema({
   usersLiked: { type: [String], req: true, default: [] },
   usersDisliked: { type: [String], req: true, default: [] },
 })
+
+sauceSchema.plugin(mongooseErrors)
 
 module.exports = mongoose.model('Sauce', sauceSchema)
