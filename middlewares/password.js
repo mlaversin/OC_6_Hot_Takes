48d5afbe0ea204cf45b5ffd0de103ea18f1ea041
Morgan Laversin <morganlaversin@gmail.com>
2022-05-23 15:37:51 +0200

    file formatting

--- middlewares/password.js
+++ middlewares/password.js
@@ -3,22 +3,13 @@ const passwordValidator = require('password-validator')
 const schema = new passwordValidator()
 
 schema
-  .is()
-  .min(8)
-  .is()
-  .max(100)
-  .has()
-  .uppercase()
-  .has()
-  .lowercase()
-  .has()
-  .digits(1)
-  .has()
-  .not()
-  .spaces()
-  .is()
-  .not()
-  .oneOf(['Passw0rd', 'Password123', 'M0tdepasse', 'motdepass3', 'motd3passe']) // Blacklist these values
+  .is().min(8)
+  .is().max(100)
+  .has().uppercase()
+  .has().lowercase()
+  .has().digits(1)
+  .has().not().spaces()
+  .is().not().oneOf(['Passw0rd', 'Password123', 'M0tdepasse', 'motdepass3', 'motd3passe']) // Blacklist these values
 
 module.exports = (req, res, next) => {
   if (schema.validate(req.body.password)) {
