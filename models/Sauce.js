8e0ab8fd43c8f23c79dc52c214a0a3289b47d162
Morgan Laversin <morganlaversin@gmail.com>
2022-05-31 19:20:53 +0200

    removes constraints on user inputs for sauce creation

--- models/Sauce.js
+++ models/Sauce.js
@@ -3,46 +3,10 @@ const mongooseErrors = require('mongoose-errors')
 
 const sauceSchema = mongoose.Schema({
   userId: { type: String, required: true },
-  name: {
-    type: String,
-    minLength: 3,
-    maxLenght: 50,
-    match: [
-      /^[a-zA-ZàâéèêëîïôùûæœÀÂÉÈÊËÎÏÔÛÙÆŒçÇñÑ '-]*$/,
-      'Character(s) not allowed in the name field',
-    ],
-    required: true,
-  },
-  manufacturer: {
-    type: String,
-    minLength: 3,
-    maxLenght: 50,
-    match: [
-      /^[a-zA-ZàâéèêëîïôùûæœÀÂÉÈÊËÎÏÔÛÙÆŒçÇñÑ '-]*$/,
-      'Character(s) not allowed in the name field',
-    ],
-    required: true,
-  },
-  description: {
-    type: String,
-    minLength: 10,
-    maxLenght: 500,
-    match: [
-      /^[a-zA-ZàâéèêëîïôùûæœÀÂÉÈÊËÎÏÔÛÙÆŒçÇñÑ '-:.?!«»/]*$/,
-      'Character(s) not allowed in the description',
-    ],
-    required: true,
-  },
-  mainPepper: {
-    type: String,
-    minLength: 3,
-    maxLenght: 50,
-    match: [
-      /^[a-zA-ZàâéèêëîïôùûæœÀÂÉÈÊËÎÏÔÛÙÆŒçÇñÑ '-]*$/,
-      'Character(s) not allowed in the main pepper ingredient field',
-    ],
-    required: true,
-  },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
   imageUrl: { type: String, required: true },
   heat: { type: Number, min: 1, max: 10, required: true },
   likes: { type: Number, required: true, default: 0 },
